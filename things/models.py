b33b294c5f7cd481fbe95ccf873f3b7c77a7c1a9
Ilia Jamasb <ilia@Ilias-Mac-mini.local>
2022-10-09 21:47:37 +0100

    Fixing task part 4

--- things/models.py
+++ things/models.py
@@ -3,17 +3,7 @@ from django.db import models
 #add namelist
 
 class Thing(models.Model):
-    if(len(models.name)>30 or len(models.name) == 0):
-        raise ValueError()
-    else:
-        self.name = models.name
 
-    if(len(models.description) > 120):
-        raise ValueError()
-    else:
-        self.description = models.description
-
-    if(models.quantity > 100 or models.quantity<0):
-        raise ValueError()
-    else:
-        self.quantity = models.quantity
+    name = models.CharField(max_length=30, unique=True)
+    description = models.CharField(max_length=120)
+    quantity = int(models.CharField())
