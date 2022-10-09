752174b6e1263ce7020b8b5edf0ea06adc7b1300
Ilia Jamasb <ilia@Ilias-Mac-mini.local>
2022-10-09 22:21:35 +0100

    Fixing task part 7

--- things/models.py
+++ things/models.py
@@ -6,4 +6,6 @@ class Thing(models.Model):
 
     name = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=120)
-    quantity = models.IntegerField(MinValueValidator = 1, MaxValueValidator = 100)
+    quantity = models.IntegerField(validators=[
+        MinValueValidator = 1,
+        MaxValueValidator = 100])
