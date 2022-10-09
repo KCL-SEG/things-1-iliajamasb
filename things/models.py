e507197ec6b6f5e143daa5b59f47ad7d31ec2e5b
Ilia Jamasb <ilia@Ilias-Mac-mini.local>
2022-10-09 21:51:21 +0100

    Fixing task part 5

--- things/models.py
+++ things/models.py
@@ -6,4 +6,4 @@ class Thing(models.Model):
 
     name = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=120)
-    quantity = int(models.CharField())
+    quantity = models.IntegerField(min_value = 1, max_value = 100)
