9d52b574b7f50fdedcf281b12450c2a108d204d8
Ilia Jamasb <ilia@Ilias-Mac-mini.local>
2022-10-09 21:14:39 +0100

    Beginning task 3

--- things/models.py
+++ things/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 
+#add namelist
+
 class Thing:
     def __init__(self, name, description, quantity):
-        self.name = name
-        self.description = description
-        self.quantity = quantity
+        if(length(name)>30 or length(name) == 0):
+            raise ValueError()
+        else:
+            self.name = name
+
+        if(length(description) > 120):
+            raise ValueError()
+        else:
+            self.description = description
+
+        if(quantity > 100 or quantity<0):
+            raise ValueError()
+        else:
+            self.quantity = quantity
