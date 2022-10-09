7458db43a021a5c7f083e66bbaa221165fcf530d
Ilia Jamasb <ilia@Ilias-Mac-mini.local>
2022-10-09 20:35:05 +0100

    Added the thing class (with description fully working)

--- things/models.py
+++ things/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class Thing:
     def __init__(self, name, description, quantity):
         self.name = name
-        self.description = desc
+        self.description = description
         self.quantity = quantity
