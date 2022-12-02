47a9695f8879d60d8d5d13668b6180124728b145
iliajamasb <99290474+iliajamasb@users.noreply.github.com>
2022-12-02 01:03:40 +0000

    this is it

--- things/models.py
+++ things/models.py
@@ -8,7 +8,7 @@ class Thing(models.Model):
     name = models.CharField(max_length=30,
     validators=[MinLengthValidator(1)], unique=True)
 
-    description = models.CharField(max_length=120)
+    description = models.CharField(max_length=120, validators=[MinLengthValidator(0)])
     quantity = models.IntegerField(validators=[
         MinValueValidator(0),
         MaxValueValidator(100)])
